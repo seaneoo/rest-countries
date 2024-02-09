ec4c8a3b2eca3001d3aaf520019b57260fe679ae
Sean O'Connor <10770201-seaneoo@users.noreply.gitlab.com>
2024-02-09 15:33:15 -0700

    Update unit test for root route

--- src/test/kotlin/dev/seano/restcountries/ApplicationTest.kt
+++ src/test/kotlin/dev/seano/restcountries/ApplicationTest.kt
@@ -1,22 +1,23 @@
 package dev.seano.restcountries
 
-import dev.seano.plugins.*
 import dev.seano.restcountries.plugins.configureRouting
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
 import io.ktor.server.testing.*
-import kotlin.test.*
+import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class ApplicationTest {
-    @Test
-    fun testRoot() = testApplication {
-        application {
-            configureRouting()
-        }
-        client.get("/").apply {
-            assertEquals(HttpStatusCode.OK, status)
-            assertEquals("Hello World!", bodyAsText())
-        }
-    }
+	@Test
+	fun testRoot() = testApplication {
+		application {
+			configureRouting()
+		}
+
+		client.get("/").apply {
+			assertEquals(HttpStatusCode.OK, status)
+			assertEquals(HttpStatusCode.OK.description, bodyAsText())
+		}
+	}
 }
