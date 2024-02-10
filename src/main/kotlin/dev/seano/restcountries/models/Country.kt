aaf25bc283d91cbbe1d1736a6d1393c0b75fafa9
Sean O'Connor <10770201-seaneoo@users.noreply.gitlab.com>
2024-02-09 17:33:22 -0700

    Create Country model

---
+++ src/main/kotlin/dev/seano/restcountries/models/Country.kt
@@ -0,0 +1,12 @@
+package dev.seano.restcountries.models
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Country(
+	val name: String,
+	@SerialName("iso_alpha2") val isoAlpha2: String,
+	@SerialName("iso_alpha3") val isoAlpha3: String,
+	@SerialName("iso_numeric") val isoNumeric: String
+)
