6f6fbe94575dfdb19b830243c3c1e72d0ff2c9f1
Sean O'Connor <10770201-seaneoo@users.noreply.gitlab.com>
2024-02-09 19:03:32 -0700

    Create application.conf

--- src/main/kotlin/dev/seano/restcountries/Application.kt
+++ src/main/kotlin/dev/seano/restcountries/Application.kt
@@ -2,28 +2,15 @@ package dev.seano.restcountries
 
 import dev.seano.restcountries.plugins.*
 import io.ktor.server.application.*
-import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 
-fun main() {
-	val env = applicationEngineEnvironment { envConfig() }
-	embeddedServer(Netty, env).start(wait = true)
-}
+fun main(args: Array<String>) = EngineMain.main(args)
 
+@Suppress("unused")
 fun Application.module() {
 	configureSerialization()
 	configureDatabases()
 	configureMonitoring()
 	configureHTTP()
 	configureRouting()
-}
-
-fun ApplicationEngineEnvironmentBuilder.envConfig() {
-	module {
-		module()
-	}
-	connector {
-		host = "0.0.0.0"
-		port = 8080
-	}
 }
